78ec39304542cfe1c48a82ce8238e20a4118f1b2
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-05-17 14:58:29 -0400

    run first test

--- test/irrep/test_irreps.py
+++ test/irrep/test_irreps.py
@@ -1,9 +1,17 @@
 import unittest
-import su2nn.irrep as irrep
+from su2nn.irrep import Irrep
 
 class Test_Irrep(unittest.TestCase):
     def test_init(self):
-        self.assertEqual(str(irrep.Irrep(1, 1, 1)), '1ee')
+        print('Test initializing Irrep')
+        self.assertEqual(str(Irrep(1, 1, 1)), '1ee')
+        self.assertEqual(str(Irrep(0.5, 1, -1)), '1/2eo')
+        self.assertEqual(str(Irrep(1.5, 'e', -1)), '3/2eo')
+        self.assertEqual(str(Irrep('1/2oe')), '1/2oe')
+        self.assertRaises(ValueError, Irrep, *(1.6, 1, 1))
+        self.assertRaises(ValueError, Irrep, *(1, 1, 2))
+        self.assertRaises(ValueError, Irrep, *(1, 2, 1))
+
 
 if __name__ == '__main__':
     unittest.main()
