18316fbc3d1aa6cdd8a2cf64a22343711a7f1963
Abhijatmedhi Chotrattanapituk <c_earth@mit.edu>
2023-07-05 03:39:55 -0400

    finish code for D_P

--- group/symmetry/P.py
+++ group/symmetry/P.py
@@ -1,17 +1,18 @@
 import torch
 
-p2num = {'o': -1, 'e': 1}
-num2p = {-1: 'o', 1: 'e'}
-
 class D_P():
     def __init__(self, p):
-        if isinstance(p, str) and p in p2num:
-            p = p2num[p]
-        
-        if p in num2p:
-            self.p = p
+        if p in [-1, 1]:
+            self._p = p
         else:
-            raise ValueError(f'Parity must be an integer -1 or 1, or a string \'o\' or \'e\', but {p} is given.')
+            raise ValueError(f'Parity must be an integer -1 or 1, but {p} is given.')
+        
+    @property
+    def p(self):
+        return self._p
+    
+    def __repr__(self):
+        return f'D_P^({self.p})'
 
     def operate(self, target, rho):
         if not isinstance(target, torch.Tensor):
